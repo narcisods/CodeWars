d01d4008383f3f027cc7977a4f28e2d8f1dcdac9
Narciso Salvador <narcisodsalvador@gmail.com>
2022-05-12 18:19:07 -0700

    updated template with PREP

--- js/0000Template.js
+++ js/0000Template.js
@@ -9,6 +9,18 @@ console.log('codewars')
 
 
 
+
+//PREP
+//PARAMETER: What can you take in?
+    //
+//RETURNS: How? print to console? Return?
+    //
+//EXAMPLES: Examples of how the code should work?
+    //
+//PSEDUO CODE: Write pseduo code for everything
+    //
+
+
 // My Solution
 
 
