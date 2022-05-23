117e4bb760fc0952329d86da6546243b315c893c
Narciso Salvador <narcisodsalvador@gmail.com>
2022-05-22 23:46:51 -0700

    added 39

---
+++ js/38ConvertaNumber.js
@@ -0,0 +1,36 @@
+console.log('codewars')
+// 5.22
+
+// Kyu 8
+// Convert a number to a string!
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/javascript
+
+//Details
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+
+
+
+//PREP
+//PARAMETER: What can you take in?
+    //
+//RETURNS: How? print to console? Return?
+    //
+//EXAMPLES: Examples of how the code should work?
+    //123 --> "123"
+//999 --> "999"
+//PSEDUO CODE: Write pseduo code for everything
+    // Use the .toString method
+
+
+// My Solution
+const numberToString = num => num.toString()  
+
+// Codewars Solution 
+
+function numberToString(num) {
+    return num.toString();
+  }
