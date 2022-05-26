c46f61d9816ed14a4402188ada80777be083e13d
Narciso Salvador <narcisodsalvador@gmail.com>
2022-05-26 08:25:25 -0700

    Added 43

---
+++ js/43Function3Multiply.js
@@ -0,0 +1,30 @@
+console.log('codewars')
+// 5.26 
+
+// Kyu 8
+// Function 3 - multiplying two numbers
+// https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+
+//Details
+// Implement a function which multiplies two numbers.
+
+
+
+
+
+//PREP
+//PARAMETER: What can you take in?
+    // 2 nums
+//RETURNS: How? print to console? Return?
+    //return
+//EXAMPLES: Examples of how the code should work?
+    //
+//PSEDUO CODE: Write pseduo code for everything
+    //
+
+
+// My Solution
+const multiply = (num1,num2) => num1 * num2
+
+// Codewars Solution 
+
