df5df1fabde6f26abbbabf437378f65aa838c3b0
Narciso Salvador <narcisodsalvador@gmail.com>
2022-05-31 15:47:08 -0700

    added 47

--- js/00Template copy.js
+++ js/00Template copy.js
@@ -1,28 +1,57 @@
 console.log('codewars')
-// Date
+// 5/31/22
 
-// Kyu?
-// Title
-// Link
+// Kyu 8
+// Lario and Muigi Pipe Problem
+
+// https://www.codewars.com/kata/56b29582461215098d00000f/train/javascript
 
 //Details
+// Issue
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
 
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+// Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+// Task
+// Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// Example
+// Input: 1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
 
 
 
 //PREP
 //PARAMETER: What can you take in?
-    //
+    // an array of numbers
 //RETURNS: How? print to console? Return?
-    //
+    // return an array
 //EXAMPLES: Examples of how the code should work?
-    //
+    // Check above
 //PSEDUO CODE: Write pseduo code for everything
     //
 
 
 // My Solution
-
+function pipeFix(numbers){
+    let result = []
+    for (let i = numbers[0];i <= numbers[numbers.length-1];i++) {
+      result.push(i)
+    }
+    return result
+  }
 
 // Codewars Solution 
+
+// function pipeFix(numbers){
+//     var first = numbers[0];
+//     var last = numbers[numbers.length-1];
+    
+//     var arr = [];
+//     for(var i = first; i <= last; i++) {
+//       arr.push(i);
+//     }
+//     return arr;
+//   }
 
