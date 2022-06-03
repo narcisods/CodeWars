97c76abdeecc6a6d49f282971a69a343c14809ed
Narciso Salvador <narcisodsalvador@gmail.com>
2022-06-02 19:40:51 -0700

    added 50

---
+++ js/50GetCharacterFrom.js
@@ -0,0 +1,30 @@
+console.log('codewars')
+// Date
+
+// Kyu 8
+// get character from ASCII Value
+
+// https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/javascript
+
+//Details
+
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+
+//PREP
+//PARAMETER: What can you take in?
+    //
+//RETURNS: How? print to console? Return?
+    //
+//EXAMPLES: Examples of how the code should work?
+    //
+//PSEDUO CODE: Write pseduo code for everything
+    //
+
+
+// My Solution
+const getChar = c => String.fromCharCode(c)
+
+// Codewars Solution 
+
+// const getChar = String.fromCharCode;
