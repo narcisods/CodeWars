3e9211f04c1535d3b5c7adfbf275996886b6c2f0
Narciso Salvador <narcisodsalvador@gmail.com>
2022-06-03 08:13:07 -0700

    added 51

---
+++ js/51HexToDec.js
@@ -0,0 +1,35 @@
+
+console.log('codewars')
+// 5.3.22
+// Kyu 8
+// Hex to Decimal
+// https://www.codewars.com/kata/57a4d500e298a7952100035d/train/javascript
+
+//Details
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+
+
+
+//PREP
+//PARAMETER: What can you take in?
+    //
+//RETURNS: How? print to console? Return?
+    //
+//EXAMPLES: Examples of how the code should work?
+    //
+//PSEDUO CODE: Write pseduo code for everything
+    //
+
+
+// My Solution
+function hexToDec(hexString){
+    return parseInt(hexString,16)
+  }
+
+// Codewars Solution 
+
+function hexToDec(hexString){
+    return  parseInt(hexString, 16);
+   }
