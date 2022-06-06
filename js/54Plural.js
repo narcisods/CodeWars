00142b4cc07c360f5931a299c44f3de31d1c31fe
Narciso Salvador <narcisodsalvador@gmail.com>
2022-06-05 23:26:53 -0700

    added 54

---
+++ js/54Plural.js
@@ -0,0 +1,32 @@
+console.log('codewars')
+// Date 6/5/22  
+
+// Kyu 8 
+// Plural
+// https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/javascript
+
+//Details
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+
+
+//PREP
+//PARAMETER: What can you take in?
+    //
+//RETURNS: How? print to console? Return?
+    //
+//EXAMPLES: Examples of how the code should work?
+    //
+//PSEDUO CODE: Write pseduo code for everything
+    //
+
+
+// My Solution
+const plural = n => n != 1 
+
+
+// Codewars Solution 
+
+// function plural(n) {
+//     return n !== 1;
+//   }
