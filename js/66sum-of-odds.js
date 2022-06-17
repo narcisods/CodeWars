01d4c1fc37c933e4a5abc6610cb9c2735daef9f0
Narciso Salvador <narcisodsalvador@gmail.com>
2022-06-17 09:42:56 -0700

    updated template

--- js/66sum-of-odds.js
+++ js/66sum-of-odds.js
@@ -42,13 +42,10 @@ const rowSumOddNumbers = n => {
  
     }
 }
-// 1 = 1
-// 2 = 2-3
-// 3 = 4-5-6
-// 4 = 7-8-9-10
-// 5 = 11-12-13-14-15
-// 6 = 16-17-18-19-20-21
 
 console.log(rowSumOddNumbers(10))
 // Codewars Solution 
 
+// function rowSumOddNumbers(n) {
+//     return Math.pow(n, 3);
+//   }
