c697b1059ccc20cd304bff19851bbb4a484711dc
Narciso Salvador <narcisodsalvador@gmail.com>
2022-07-03 10:06:14 -0700

    updated

--- js/84growthofapopulation.js
+++ js/84growthofapopulation.js
@@ -41,4 +41,11 @@ function nbYear(population, percent, addition,target) {
 }
 
 // Codewars Solution 
-
+// function nbYear(p0, percent, aug, p) {
+    
+//     for (var years = 0; p0 < p; years++) {
+//       p0 = Math.floor(p0 + p0 * percent / 100 + aug);
+//     }
+//     return years
+//   }
+// 
