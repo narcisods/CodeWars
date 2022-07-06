4f244fc725892ac0613e70e5fd16317574dcb9b5
Narciso Salvador <narcisodsalvador@gmail.com>
2022-07-06 11:02:05 -0700

    updated 85

--- js/85mexicanwave.js
+++ js/85mexicanwave.js
@@ -50,3 +50,14 @@ console.log(wave('hello world'))
 
 // Codewars Solution 
 
+// function wave(str){
+//     let result = [];
+    
+//     str.split("").forEach((char, index) => {
+//         if (/[a-z]/.test(char)) {
+//             result.push(str.slice(0, index) + char.toUpperCase() + str.slice(index + 1));
+//         }
+//     });
+    
+//     return result;
+// }
