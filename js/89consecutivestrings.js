9d2765512b7ee5b5bdef9b7d7302fe8dd87664f7
Narciso Salvador <narcisodsalvador@gmail.com>
2022-07-10 11:07:09 -0700

    updated 89

--- js/89consecutivestrings.js
+++ js/89consecutivestrings.js
@@ -67,3 +67,14 @@ function longestConsec(starrr,k) {
 console.log(longestConsec(["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], 2))
 // Codewars Solution 
 
+// function longestConsec(strarr, k) {
+//     var longest = "";
+//     for(var i=0;k>0 && i<=strarr.length-k;i++){
+//       var tempArray = strarr.slice(i,i+k);
+//       var tempStr = tempArray.join("");
+//       if(tempStr.length > longest.length){
+//         longest = tempStr;
+//       }
+//     }
+//     return longest;
+// }
