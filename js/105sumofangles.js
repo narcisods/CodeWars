c322ee9aa5ab1b66cb23b2a5d689acb29896e53b
Narciso Salvador <narcisodsalvador@gmail.com>
2022-07-28 10:02:48 -0700

    added 105

---
+++ js/105sumofangles.js
@@ -0,0 +1,33 @@
+console.log('codewars test')
+// 7/28 
+
+// 7 Kyu
+// Sum of angles
+// https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/javascript
+
+//Details
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+
+
+//PREP
+//PARAMETER: What can you take in?
+    // A number greater than 2
+//RETURNS: How? print to console? Return?
+    // Return a number/integer
+//EXAMPLES: Examples of how the code should work?
+    //     assert.strictEqual(angle(3), 180);
+    // assert.strictEqual(angle(4), 360);
+//PSEDUO CODE: Write pseduo code for everything
+    // Return the formula 
+
+
+// My Solution
+const angle= n => (n-2)*180
+
+
+// Codewars Solution 
+
+// function angle(n) {
+//     return 180*(n-2);
+//   }
