1f1e7560716540b5e07c8154ff5670117b6551e2
Narciso Salvador <narcisodsalvador@gmail.com>
2022-08-06 17:29:41 -0700

    added 111

---
+++ js/111deleteoccurences.js
@@ -0,0 +1,54 @@
+console.log('codewars test')
+// 8/7
+
+// 6 Kyu
+// Delete occurrences of an element if it occurs more than n times
+// https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/javascript
+
+//Details
+// Enough is enough!
+// Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motif usually repeats. He isn't fond of seeing the Eiffel tower 40 times.
+// He tells them that he will only sit for the session if they show the same motif at most N times. Luckily, Alice and Bob are able to encode the motif as a number. Can you help them to remove numbers such that their list contains each number only up to N times, without changing the order?
+
+
+// Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+// For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+// With list [20,37,20,21] and number 1, the result would be [20,37,21].
+
+
+//PREP
+//PARAMETER: What can you take in?
+    // An array of numbers and a postivie integer, n
+//RETURNS: How? print to console? Return?
+    // Return an array of numbers
+//EXAMPLES: Examples of how the code should work?
+    // ([20,37,20,21], 1), [20,37,21])
+//PSEDUO CODE: Write pseduo code for everything
+    // 
+
+
+// My Solution
+// function deleteNth(arr,n) {
+//     const count = {}
+//     arr.forEach(element=>{
+//         count[element] = (count[element] || 0) +1
+//     })
+//     arr.map(element )
+
+// }
+
+//Test
+
+
+// Codewars Solution 
+function deleteNth(arr,x) {
+    var cache = {};
+    return arr.filter(function(n) {
+      cache[n] = (cache[n]||0) + 1;
+        console.log(cache)
+      return cache[n] <= x;
+    });
+  }
+
+console.log(deleteNth([20,37,20,21],1))
+console.log(deleteNth([1,1,3,3,7,2,2,2],3))
