873f13598a36ed3fd8b03bc409f8ffe7ca0f4c3f
Narciso Salvador <narcisodsalvador@gmail.com>
2022-08-09 09:01:54 -0700

    added 114

---
+++ js/114stringcleaning.js
@@ -0,0 +1,28 @@
+console.log('codewars test')
+// Date
+
+// ? Kyu
+// Title
+// Link
+
+//Details
+
+
+
+
+//PREP
+//PARAMETER: What can you take in?
+    //
+//RETURNS: How? print to console? Return?
+    //
+//EXAMPLES: Examples of how the code should work?
+    //
+//PSEDUO CODE: Write pseduo code for everything
+    //
+
+
+// My Solution
+
+
+// Codewars Solution 
+
