920ded89d9076b4d0e4a324270762f58cd3fa4a1
Narciso Salvador <narcisodsalvador@gmail.com>
2022-08-13 15:36:06 -0700

    typo

--- js/118awolfinsheepsclothing.js
+++ js/118awolfinsheepsclothing.js
@@ -56,4 +56,4 @@ const warnTheSheep = queue => {
 // function warnTheSheep(queue) {
 //     const position = queue.reverse().indexOf('wolf');
 //     return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
-//   }
+//   } 
