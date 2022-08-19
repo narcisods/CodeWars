c17f5ab278186256a1f4fe66e1e623238fe387c9
Narciso Salvador <narcisodsalvador@gmail.com>
2022-08-19 15:34:13 -0700

    updated

--- js/00Template.js
+++ js/00Template.js
@@ -16,9 +16,10 @@ console.log('codewars test')
 
 //RETURNS: How? print to console? Return?
     //
-    
+
 //EXAMPLES: Examples of how the code should work?
     //
+    
 //PSEDUO CODE: Write pseduo code for everything
     //
 
