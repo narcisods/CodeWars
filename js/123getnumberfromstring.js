c4c949cc1ff486e6dd060fb42095c533e50337ce
Narciso Salvador <narcisodsalvador@gmail.com>
2022-08-27 14:55:50 -0700

    added

---
+++ js/123getnumberfromstring.js
@@ -0,0 +1,35 @@
+console.log('Codewars test')
+// 08/29
+
+// 8 Kyu
+// Get number from string
+// https://www.codewars.com/kata/57a37f3cbb99449513000cd8/train/javascript
+
+//Details
+
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+// Function:
+
+// getNumberFromString(s)
+
+
+//PREP
+//PARAMETER: What can you take in?
+    // A string
+
+//RETURNS: How? print to console? Return?
+    // Return 
+
+//EXAMPLES: Examples of how the code should work?
+    //
+
+//PSEDUO CODE: Write pseduo code for everything
+    //
+
+
+// My Solution
+
+
+// Codewars Solution 
+
