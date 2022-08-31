37a2e26706378957459d1b5077f49af8b02243a1
Narciso Salvador <narcisodsalvador@gmail.com>
2022-08-31 15:36:22 -0700

    added 125

---
+++ js/125simplecomparison.js
@@ -0,0 +1,40 @@
+console.log('Codewars test')
+// Date
+
+// 8 Kyu
+// Simple Comparison?
+// https://www.codewars.com/kata/57f6ecdfcca6e045d2001207/train/javascript
+
+//Details
+// Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+// To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
+
+
+
+//PREP
+//PARAMETER: What can you take in?
+    // A string and or number   
+
+//RETURNS: How? print to console? Return?
+    // Return a boolean
+
+//EXAMPLES: Examples of how the code should work?
+    // 
+
+//PSEDUO CODE: Write pseduo code for everything
+    //
+
+
+// My Solution
+function add(a, b){
+	return a == b
+}
+
+
+
+// Codewars Solution 
+
+function add(a, b){
+    return a == b 
+  }
