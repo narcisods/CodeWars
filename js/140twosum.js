3233c52afe688c1ba5f9f88daf17ec530301ea70
Narciso Salvador <narcisodsalvador@gmail.com>
2022-10-08 13:26:24 -0700

    updated 140

--- js/140twosum.js
+++ js/140twosum.js
@@ -47,10 +47,10 @@ console.log(twoSum([1,2,3],4))
 
 // Codewars Solution 
 
-function twoSum(numbers, target) {
-    for (var i = 0; i < numbers.length-1; i++) {
-        for (var j = i+1; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === target) return [i, j];
-        }
-    }
-}
+// function twoSum(numbers, target) {
+//     for (var i = 0; i < numbers.length-1; i++) {
+//         for (var j = i+1; j < numbers.length; j++) {
+//             if (numbers[i] + numbers[j] === target) return [i, j];
+//         }
+//     }
+// }
