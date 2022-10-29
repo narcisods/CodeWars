5e9e64f44880382a829c5dcc95e829f7b1d811b6
Narciso Salvador <narcisodsalvador@gmail.com>
2022-10-29 10:12:55 -0700

    updated

--- js/dailychallenges/sort.js
+++ js/dailychallenges/sort.js
@@ -20,4 +20,4 @@ const sortArray = (arr) => {
 
 console.log(nthSmallest([3, 1, 2], 2), 2);
 console.log(nthSmallest([15, 20, 7, 10, 4, 3], 3), 7);
-console.log(nthSmallest([177, 225, 243, -169, -12, -5, 2, 92], 5), 92);
+// console.log(nthSmallest([177, 225, 243, -169, -12, -5, 2, 92], 5), 92);
