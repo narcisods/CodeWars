269e562516f2fa91ccaf58c7367728ff69f884a6
Narciso Salvador <narcisodsalvador@gmail.com>
2022-11-16 08:38:33 -0800

    updated

--- js/dailychallenges/structy/binaryTreePractice.js
+++ js/dailychallenges/structy/binaryTreePractice.js
@@ -379,3 +379,90 @@ console.log('Node Class enabled');
 // };
 
 // console.log(howHigh(a)); // -> 2
+// bottom right value
+// Write a function, bottomRightValue, that takes in the root of a binary tree. The function should return the right-most value in the bottom-most level of the tree.
+
+// You may assume that the input tree is non-empty.
+
+// test_00:
+// const a = new Node(3);
+// const b = new Node(11);
+// const c = new Node(10);
+// const d = new Node(4);
+// const e = new Node(-2);
+// const f = new Node(1);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+
+//       3
+//    /    \
+//   11     10
+//  / \      \
+// 4   -2     1
+
+// const bottomRightValue = (root) => {
+// 	if (root === null) return null;
+// 	const queue = [root];
+// 	let current = null;
+// 	while (queue.length > 0) {
+// 		current = queue.shift();
+
+// 		if (current.left) queue.push(current.left);
+// 		if (current.right) queue.push(current.right);
+// 	}
+// 	return current.val;
+// };
+// bottomRightValue(a); // -> 1
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e
+// const allTreePaths = (root) => {
+// 	if (root === null) return null;
+// 	const stack = [root];
+// 	let currentPath = [];
+// 	const path = [];
+// 	while (stack.length > 0) {
+// 		const current = stack.pop();
+// 		currentPath.push(current.val);
+
+// 		if (current.right) stack.push(current.right);
+// 		if (current.left) stack.push(current.left);
+// 		if (current.right === null && current.left === null) {
+// 			path.push(currentPath);
+// 			currentPath = [];
+// 		}
+// 	}
+// 	return path;
+// };
+
+const allTreePaths = (root) => {
+	if (root === null) return [];
+	if (root.left === null && root.right === null) return [[root.val]];
+	return root.push(allTreePaths(root.left) + allTreePaths(root.right));
+};
+console.log(allTreePaths(a)); // ->
+// [
+//   [ 'a', 'b', 'd' ],
+//   [ 'a', 'b', 'e' ],
+//   [ 'a', 'c', 'f' ]
+// ]
+//
