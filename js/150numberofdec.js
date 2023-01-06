11be51b30730b3905e1f86f0852581cfc9234ffd
Narciso Salvador <narcisodsalvador@gmail.com>
2023-01-06 09:38:26 -0800

    fixed

--- js/150numberofdec.js
+++ js/150numberofdec.js
@@ -1,31 +1,34 @@
-console.log('Codewars test')
-// Date
+console.log('Codewars test');
+// 1/6
 
-// ? Kyu
-// Title
-// Link
+// 7 Kyu
+// Number of Decimal Digits
+// https://www.codewars.com/kata/58fa273ca6d84c158e000052/train/javascript
 
 //Details
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
 
-
-
+// All inputs will be valid.
 
 //PREP
 //PARAMETER: What can you take in?
-    //
+// num
 
 //RETURNS: How? print to console? Return?
-    //
+// return
 
 //EXAMPLES: Examples of how the code should work?
-    //
+//
 
 //PSEDUO CODE: Write pseduo code for everything
-    //
-
+//
 
 // My Solution
+function digits(n) {
+	return n.toString().length;
+}
+// Codewars Solution
 
-
-// Codewars Solution 
-
+// function digits(n) {
+//     return n.toString().length;
+//   }
