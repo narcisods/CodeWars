bc6bb422263dcde0b5d9e4c403cf671ce0b61539
Narciso Salvador <narcisodsalvador@gmail.com>
2023-02-20 10:29:46 -0800

    updated fizzBuzz

--- js/dailychallenges/structy/test.js
+++ js/dailychallenges/structy/test.js
@@ -1,1 +1,13 @@
-function fizzBuzz() {}
+const fizzBuzz = (num) => {
+	for (let i = 0; i < num; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log('fizzBuzz');
+		} else if (i % 3 === 0) {
+			console.log('fizz');
+		} else if (i % 5 === 0) {
+			console.log('buzz');
+		} else {
+			console.log(i);
+		}
+	}
+};
